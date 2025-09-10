ce293955bea21e5f41d388887addf4edb1ce073e
sanderhelg <sander@icgi.no>
2025-09-10 10:27:55 +0200

    Update run_training_and_inference.sh

--- run_training_and_inference.sh
+++ run_training_and_inference.sh
@@ -1,3 +1,1 @@
-nnUNetv2_train 9 2d all -p nnUNetResEncUNetLPlans && \
-echo "Training completed successfully" || { echo "Training failed"; exit 1; }
-nnUNetv2_predict -i /mnt/raid0/nnUNet_raw/Dataset009_TissueSegmentation_final/imagesTs/ -o /mnt/raid0/tissueSegmentation_ResEnc_final_test_data_output -d 9 -c 2d -f all -p nnUNetResEncUNetLPlans
+nnUNet_predict_tissue -i /path/to/files/or/scan_list_txt -o /path/to/output/files 
